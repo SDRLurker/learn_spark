fbe92918c234dfa1ddad243a81205e839e9d655c
SDRLurker <1982sdr@hanmail.net>
2018-01-14 01:53:34 +0900

    learn_spark 20180113 - java.sh JAVA_HOME, PATH를 .bashrc에 추가하도록 변경.

--- spark_install/java.sh
+++ spark_install/java.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 mkdir -p $HOME/apps
 cd $HOME/apps
-wget --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"
-tar xzf jdk-8u151-linux-x64.tar.gz
+wget --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz"
+tar xzf jdk-8u152-linux-x64.tar.gz
 
 rm -rf jdk
-ln -s jdk1.8.0_151 jdk
+ln -s jdk1.8.0_152 jdk
 
-rm -rf $HOME/apps/jdk-8u151-linux-x64.tar.gz
+rm -rf $HOME/apps/jdk-8u152-linux-x64.tar.gz
 
 JAVA_HOME=$HOME/apps/jdk
-echo "export JAVA_HOME=$HOME/apps/jdk" >> ~/.bash_profile
-echo "export PATH=${JAVA_HOME}/bin:$PATH" >> ~/.bash_profile
+echo "export JAVA_HOME=$HOME/apps/jdk" >> ~/.bashrc
+echo "export PATH=${JAVA_HOME}/bin:$PATH" >> ~/.bashrc
 
-source ~/.bash_profile
+source ~/.bashrc
